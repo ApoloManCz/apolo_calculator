45922bffe766626b543148ec6b1054a1d2698632
Stroudy <32533859+STR0UDY@users.noreply.github.com>
2022-07-01 15:24:32 +0100

    feat(nui): Copy the result to clipboard + rdr3 support

--- client/main.lua
+++ client/main.lua
@@ -1,27 +1,23 @@
 RegisterCommand("calculate", function(source, args, rawCommand) -- Edit command name
+	local first_arg = args[1]
+	local action = args[2]
+	local second_arg = args[3]
+	local result = Quantity(first_arg, action, second_arg)
 
-local first_arg = args[1]
-local action = args[2]
-local second_arg = args[3]
-local result = Quantity(first_arg, action, second_arg)
-
-    exports['mythic_notify']:DoHudText('inform', 'Result: '..result) -- Edit to your Notify system
-
+	SendNUIMessage({ string = result })
+	exports["mythic_notify"]:DoHudText("inform", "Result: " .. result) -- Edit to your Notify system
 end)
 
 function Quantity(first_arg, action, second_arg)
-    if action == '+' then
-        return first_arg + second_arg
-    elseif action == '-' then
-        return first_arg - second_arg
-    elseif action == '*' then
-        return first_arg * second_arg
-    elseif action == '/' then
-        return first_arg / second_arg
-    else
-        exports['mythic_notify']:DoHudText('error', 'Invalid Quantity!') -- Edit to your Notify system
-    end
+	if action == "+" then
+		return first_arg + second_arg
+	elseif action == "-" then
+		return first_arg - second_arg
+	elseif action == "*" then
+		return first_arg * second_arg
+	elseif action == "/" then
+		return first_arg / second_arg
+	else
+		exports["mythic_notify"]:DoHudText("error", "Invalid Quantity!") -- Edit to your Notify system
+	end
 end
-
-
-    
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,15 @@
-fx_version 'adamant'
-game 'gta5'
+fx_version("cerulean")
+game({ "gta5", "rdr3" })
 
-author 'Apolo'
-description 'Calculator basic script'
-version '1.0.0'
+ui_page("html/index.html")
 
-client_script 'client/main.lua'
+author("Apolo")
+description("Calculator basic script")
+version("1.0.0")
+
+client_script("client/main.lua")
+
+files({
+	"html/index.html",
+	"html/index.js",
+})
